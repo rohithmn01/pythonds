6bf88b0bad52bcda96a96e2fb4aa4278a997286d
i346327 <rohith.m.n@sap.com>
2022-06-10 22:29:15 +0530

    functions

--- 04-Functions/arguments.py
+++ 04-Functions/arguments.py
@@ -1,1 +1,15 @@
+def add(*args):
+    sum=0
+    for i in args:
+        sum = sum + i
+    return sum
 
+res = add(1,2,3,4)
+print(res)
+
+
+def printKWArgs(**kwargs):
+    print(kwargs)
+
+printKWArgs(server='localhost', port=3306, user='root', password='xxxxxxxxxxxxx')
+#Note: Always place **kwargs at the end of the function
---
+++ 04-Functions/type_hints.py
@@ -0,0 +1,7 @@
+from typing import List
+
+def typeHint(i:int, s:str, l:List) -> str:
+    pass
+
+
+typeHint(333, "rohith", [1,2,3])
